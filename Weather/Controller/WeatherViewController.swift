8f1be8af083a5905394c9a7df955e9cb137ded1a
Aleksandr802 <savs802@gmail.com>
2021-01-12 11:35:15 +0200

    Initial Commit

---
+++ Weather/Controller/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Aleksandr Seminov on 12/01/2021.
+//  Copyright © 2021 Aleksandr Seminov. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
